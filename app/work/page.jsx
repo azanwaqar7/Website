11ca15065f4cfcb6e634a292f7c55e5397473915
azanwaqar7 <azanwaqar7@gmail.com>
2025-07-29 01:38:58 +0500

    Update website with latest changes

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -53,11 +53,11 @@ const projects = [
         category: 'Food & Beverage Dashboard',
         title: 'project 3',
         description: 'Developed interactive dashboards for Brand and Product Dossiers, as well as Trends and Rankings, enabling data-driven insights across key FMCG metrics. The Brand/Product Dossier includes performance, scorecards, demographics, peer comparisons, and customer feedback through ratings and reviews. Additionally, the Trends and Rankings module features dynamic dashboards such as the Search & Order Trend Monitor, Category Insight, and Brand Intelligence, helping stakeholders identify market movements, category performance, and brand positioning in real time.',
-        stack: [{name: "SQL"},{name: "Excel"}, {name: "Sisense"}, {name: "Amazon QuickSight"} , {name: "Amazon Redshift SQL"}  ],
+        stack: [{name: "SQL"}, {name: "Sisense"},{name: "Excel"}, {name: "Amazon QuickSight"} , {name: "Amazon Redshift SQL"}  ],
         image: '/assets/work/foodbev.png',
-        live:"/assets/work/Portfolio_compressed.pdf",
+        live:"https://drive.google.com/file/d/12pWZBlaYabaNrHhd6g7x8Txi25j1-5C_/view?usp=sharing",
         github: "https://github.com/azanwaqar7",
-        pdf: "/assets/work/Portfolio_compressed.pdf"
+        pdf: "https://drive.google.com/file/d/12pWZBlaYabaNrHhd6g7x8Txi25j1-5C_/view?usp=sharing"
 
     },
 
